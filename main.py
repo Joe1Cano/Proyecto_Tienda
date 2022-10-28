775e8819f328f227adb2ce5e2fe37bcc75540747
Joe1Cano <canojoel55@hotmail.com>
2022-10-28 10:08:31 -0500

    conexion con mysql

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 from flask import Flask
 from flask import render_template
+from flaskext.mysql import MySQL
 
 
 app= Flask(__name__)
 
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'tienda'
+mysql.init_app(app)
+
+
+
 @app.route("/")
 def index():
+
+    sql ="INSERT into clientes (nombre, apellido, telefono, correo) VALUES ('Joel', 'Cano', '1232141561',  '123@hotmail.com');"
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
+
     return render_template("empleados/index.html")
 
 
