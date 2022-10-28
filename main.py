7c7a0c9d48d6791ec181234850bda4c9e21bcaf5
Joe1Cano <canojoel55@hotmail.com>
2022-10-28 11:04:57 -0500

    enviar datos a sql con el formulario

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 from flaskext.mysql import MySQL
 
 
@@ -12,22 +12,35 @@ app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'tienda'
 mysql.init_app(app)
 
-
-
 @app.route("/")
 def index():
-
-    sql =""
-
+    """sql =""
     conn = mysql.connect()
-    cursor = conn.cursor()
+    cursor = conn.cursor() 
     cursor.execute(sql)
-    conn.commit()
-    return render_template("empleados/index.html")
+    conn.commit()"""
+    return render_template("clientes/index.html")
 
 @app.route('/create')
 def create():
-    return render_template('empleados/create.html')
+    return render_template('clientes/create.html')
+
+@app.route('/store', methods=['POST'])
+def storage():
+    _nombre=request.form["txtNombre"]
+    _apellido=request.form["txtApellido"]
+    _correo=request.form["txtCorreo"]
+    _telefono=request.form["txtTelefono"]
+
+    sql ="INSERT into clientes (nombre, apellido, telefono, correo) VALUES (%s, %s, %s, %s);"
+
+    datos=(_nombre,_apellido,_telefono,_correo)
+    conn = mysql.connect()
+    cursor = conn.cursor() 
+    cursor.execute(sql, datos)
+    conn.commit()
+    return render_template("clientes/index.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
