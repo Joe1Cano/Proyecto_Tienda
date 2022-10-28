31103e21540082ac5a1359abde6dee183845ccfa
Joe1Cano <canojoel55@hotmail.com>
2022-10-28 09:50:33 -0500

    abre el html

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask import render_template
 
 
-app= Flask(_name_)
+app= Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("empleados/index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
