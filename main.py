46344d97b03136fb321137b961e77335b1633f5c
Joe1Cano <canojoel55@hotmail.com>
2022-11-11 10:56:27 -0600

    crud productos listo :)

--- main.py
+++ main.py
@@ -45,19 +45,29 @@ def indexC():
     return render_template("clientes/index.html", clientes=clientes )
 
 @app.route('/clientes/destroy/<int:id>')
-def destroy(id):
+def destroyC(id):
     conn = mysql.connect()
     cursor = conn.cursor() 
     cursor.execute("DELETE FROM clientes WHERE noClientes = %s",(id))
     conn.commit()
     return redirect('/clientes')
 
+@app.route('/productos/destroy/<int:id>')
+def destroyP(id):
+    conn = mysql.connect()
+    cursor = conn.cursor() 
+    cursor.execute("DELETE FROM productos WHERE noProductos = %s",(id))
+    conn.commit()
+    return redirect('/productos')
+
 @app.route('/clientes/create')
-def create():
+def createC():
     return render_template('clientes/create.html')
 
+
+
 @app.route('/clientes/update', methods=['POST'])
-def update():
+def updateC():
     _nombre=request.form["txtNombre"]
     _apellido=request.form["txtApellido"]
     _correo=request.form["txtCorreo"]
@@ -73,8 +83,30 @@ def update():
     conn.commit()
     return redirect('/clientes')
 
+@app.route('/productos/update', methods=['POST'])
+def updateP():
+    _Nombre = request.form["txtNombre"]
+    _Precio = request.form["txtPrecio"]
+    _Cantidad = request.form["txtCantidad"]
+    _noProveedor = request.form["txtProveedor"]
+    _Costo = request.form["txtCosto"]
+    id=request.form["txtID"]
+
+    sql ="UPDATE productos set nombre=%s, precio=%s, cantidad=%s, noProveedor=%s, costo=%s where noProductos =%s;"
+    datos=(_Nombre,_Precio,_Cantidad,_noProveedor,_Costo, id)
+    
+    conn = mysql.connect()
+    cursor = conn.cursor() 
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/productos')
+
+@app.route('/productos/create')
+def createP():
+    return render_template('productos/create.html')
+
 @app.route('/clientes/edit/<int:id>')
-def edit(id):
+def editC(id):
     conn = mysql.connect()
     cursor = conn.cursor() 
     cursor.execute("SELECT * FROM clientes WHERE noClientes =%s", (id))
@@ -85,8 +117,20 @@ def edit(id):
     conn.commit()
     return render_template('clientes/edit.html', clientes=clientes)
 
+@app.route('/productos/edit/<int:id>')
+def editP(id):
+    conn = mysql.connect()
+    cursor = conn.cursor() 
+    cursor.execute("SELECT * FROM productos WHERE noProductos =%s", (id))
+
+    productos = cursor.fetchall()
+    print(productos)
+
+    conn.commit()
+    return render_template('productos/edit.html', productos=productos)
+
 @app.route('/clientes/store', methods=['POST'])
-def storage():
+def storageC():
     _nombre=request.form["txtNombre"]
     _apellido=request.form["txtApellido"]
     _correo=request.form["txtCorreo"]
@@ -94,7 +138,7 @@ def storage():
 
     if _nombre=="" or _apellido=="" or _correo =="" or _telefono =="":
         flash('Tienes que llenar todos los campos')
-        return redirect(url_for("create"))
+        return redirect(url_for("clientes/create"))
 
     sql ="call altaClientes(%s, %s, %s, %s);"
 
@@ -104,6 +148,27 @@ def storage():
     cursor.execute(sql, datos)
     conn.commit()
     return redirect('/clientes')
+
+@app.route('/productos/store', methods=['POST'])
+def storageP():
+    _Nombre = request.form["txtNombre"]
+    _Precio = request.form["txtPrecio"]
+    _Cantidad = request.form["txtCantidad"]
+    _noProveedor = request.form["txtProveedor"]
+    _Costo = request.form["txtCosto"]
+
+    if _Nombre=="" or _Precio=="" or _Cantidad =="" or _noProveedor =="" or _Costo=="":
+        flash('Tienes que llenar todos los campos')
+        return redirect(url_for("create"))
+
+    sql ="INSERT INTO productos (nombre, precio, cantidad, noProveedor, costo) VALUES (%s,%s,%s,%s,%s);"
+
+    datos=(_Nombre,_Precio,_Cantidad,_noProveedor,_Costo)
+    conn = mysql.connect()
+    cursor = conn.cursor() 
+    cursor.execute(sql, datos)
+    conn.commit()
+    return redirect('/productos')
 
 
 if __name__ == '__main__':
